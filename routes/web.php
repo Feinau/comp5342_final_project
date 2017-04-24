9f0fe30041d05cc650363482327fc6c45ed71032
Fei Teng <fei@proposalnet.com>
2017-04-24 00:11:17 -0500

    create project

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin_page', function () {
+
+    //dd(DB::select('SELECT * FROM test_data'));
+    return view('admin_page');
+});
